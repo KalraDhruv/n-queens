50fc66a8c670e3e58187db6c33c48eb8b48c0acc
KalraDhruv <dhruvkalra@proton.me>
2024-06-06 17:49:06 +0000

    Added descriptions for the methods

--- nQueen.java
+++ nQueen.java
@@ -5,8 +5,15 @@ public class nQueen {
         System.out.print("Type the number of squares on the board: ");
         Scanner scanner = new Scanner(System.in);
         int squares = scanner.nextInt();
+        int[][] board = boardCreation(squares);
 
     }
+
+    /** Here 2 invariants are used that are:
+     *  1. The empty spaces are going to be represented by 0.
+     *  2. The spaces where a queen is present would be represented by 1.
+     * @param board
+     */
     public static void boardRepresentation(int[][] board){
        for(int i=0;i<board.length;i++){
            System.out.print("{ ");
@@ -20,8 +27,19 @@ public class nQueen {
            System.out.print(" }");
        }
     }
-    public static int[][] boardCreation(int n){
 
+    /** This method creates a board with all values initialized to 0.
+     * @param n
+     * @return
+     */
+    public static int[][] boardCreation(int n){
+        int[][] board = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]= 0;
+            }
+        }
+        return board;
     }
 
 }
